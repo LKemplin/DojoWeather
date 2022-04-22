604af77af4c2e0f150b8e7a455b4cf3b7f42a019
Lynsey Kemplin <purplez479@gmail.com>
2022-04-21 18:20:14 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function remove(id) {
+    var element = document.querySelector(id)
+    element.remove();
+}
+
+function loading() {
+    console.log("loading weather report...")
+    alert("Loading weather report...");
+}
